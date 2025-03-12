660170cd56ed610f4cefaca79a0a579a0efd56ea
arisa-echt3d <arisa@echt3d.ch>
2025-03-12 11:58:25 +0100

    created the header for desktop

---
+++ app/components/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  const navItems = ["Projekt", "Lage", "Kontakt"];
+  return (
+    <header className=" h-header bg-white shadow-md w-full">
+      <div className="max-w-desktop mx-auto h-full flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logos/logo.svg"
+            width={263}
+            height={60}
+            alt="Aurora Logo"
+          />
+        </Link>
+        <nav>
+          <ul className="flex font-bold gap-12">
+            {navItems.map((navItem, i) => (
+              <li
+                key={i}
+                className="hover:bg-primary hover:text-white py-2 px-4 duration-300 rounded-3xl"
+              >
+                <Link href={`#${navItem}`}>{navItem}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
