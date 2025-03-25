71d51f6334761ca4914a6ef56f9b0f57400fc26c
arisa-echt3d <arisa@echt3d.ch>
2025-03-25 15:55:00 +0100

    added more margin in the button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,7 +4,7 @@ const Button = () => {
   return (
     <Link
       href="#Kontakt"
-      className="self-end xl:mr-20 3xl:mr-32 h-button w-button bg-primary hover:bg-accent -rotate-12 hover:rotate-0 duration-300 p-6 rounded-full text-center text-white hidden xl:flex justify-center items-center font-bold"
+      className="self-end xl:mr-32 h-button w-button bg-primary hover:bg-accent -rotate-12 hover:rotate-0 duration-300 p-6 rounded-full text-center text-white hidden xl:flex justify-center items-center font-bold"
     >
       <p>Neugierig geworden? Schreib uns gern!</p>
     </Link>
