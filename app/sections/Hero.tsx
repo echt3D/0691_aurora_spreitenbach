71c7e9e1ddd00c18e946b0e79825e0c6ace51b66
arisa-echt3d <arisa@echt3d.ch>
2025-03-25 13:40:51 +0100

    adjusted the half width

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="w-full xl:h-[120vh] flex flex-col-reverse xl:flex-row bg-tertiary">
-      <aside className="max-w-mobile md:max-w-tablet xl:max-w-desktop_half mx-auto xl:pt-header flex flex-col justify-center gap-8 py-8 pb-16 xl:py-0">
+    <section className="w-full xl:h-[120vh] flex flex-col-reverse xl:flex-row bg-tertiary xl:gap-20">
+      <aside className="max-w-mobile md:max-w-tablet xl:max-w-desktop_half ml-auto  xl:pt-header flex flex-col justify-center gap-8 py-8 pb-16 xl:py-0">
         <div className="text-accent ">
           <p className="text-h4_mobile xl:text-h4_desktop">Willkommen</p>
           <h1
@@ -33,12 +33,12 @@ hyphens-auto"
         </div>
       </aside>
       <aside className="z-10  h-[80vh] md:h-[100vh] xl:h-full flex justify-end">
-        <div className="relative w-[100vw] h-full  xl:w-[45vw]  xl:max-w-7xl xl:mx-auto  lg:h-auto ">
+        <div className="relative  h-full w-[92vw] xl:w-[45vw]  xl:max-w-7xl xl:mx-auto ">
           <Image
             src="/images/main_visu.png"
             fill
             alt="aurora spreitenbach main visualisierung"
-            className="object-cover md:object-contain"
+            className="object-contain md:object-cover xl:object-contain"
           />
         </div>
       </aside>
