c10324ec34b74d5cf6498166c4f52f957bd471a4
arisa-echt3d <arisa@echt3d.ch>
2025-03-25 14:34:49 +0100

    adjusted the hero

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="w-full xl:h-[120vh] flex flex-col-reverse xl:flex-row bg-tertiary xl:gap-20">
-      <aside className="max-w-mobile md:max-w-tablet xl:max-w-desktop_half ml-auto  xl:pt-header flex flex-col justify-center gap-8 py-8 pb-16 xl:py-0">
+    <section className="relative w-full flex flex-col-reverse xl:flex-row bg-tertiary  ">
+      <aside className="max-w-mobile md:max-w-tablet xl:max-w-desktop_half mx-auto xl:pt-header flex flex-col justify-center gap-8 py-8 pb-16 xl:py-0">
         <div className="text-accent ">
           <p className="text-h4_mobile xl:text-h4_desktop">Willkommen</p>
           <h1
@@ -32,15 +32,14 @@ hyphens-auto"
           </p>
         </div>
       </aside>
-      <aside className="z-10  h-[80vh] md:h-[100vh] xl:h-full flex justify-end">
-        <div className="relative  h-full w-[92vw] xl:w-[45vw]  xl:max-w-7xl xl:mx-auto ">
-          <Image
-            src="/images/main_visu.png"
-            fill
-            alt="aurora spreitenbach main visualisierung"
-            className="object-contain md:object-cover xl:object-contain"
-          />
-        </div>
+      <aside className="z-10 h-full flex justify-end xl:justify-center ">
+        <Image
+          src="/images/main_visu.png"
+          width={800}
+          height={1000}
+          alt="aurora spreitenbach main visualisierung"
+          className=""
+        />
       </aside>
     </section>
   );
