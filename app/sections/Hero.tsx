decfb099091256c68834e19517abc48ad94ada0c
arisa-echt3d <arisa@echt3d.ch>
2025-04-11 11:24:38 +0200

    created text.json

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,34 +1,32 @@
 import Image from "next/image";
+import t from "../dics/text.json";
 
 const Hero = () => {
+  const { main_hero } = t.home;
   return (
     <section className="relative w-full flex flex-col-reverse xl:flex-row bg-tertiary  ">
       <aside className="max-w-mobile md:max-w-tablet xl:max-w-desktop_half mx-auto xl:pt-header flex flex-col justify-center gap-8 py-8 pb-16 xl:py-0">
         <div className="text-accent ">
-          <p className="text-h4_mobile xl:text-h4_desktop">Willkommen</p>
+          <p className="text-h4_mobile xl:text-h4_desktop">{main_hero.lead}</p>
           <h1
             className="text-h1_mobile xl:text-h1_desktop leading-15 font-light
 hyphens-auto"
             lang="de"
           >
-            Dein neues
+            {main_hero.title.one}
             <br />
-            <span className="bg-primary text-white px-2">Zuhause</span> wartet
-            auf dich!
+            <span className="bg-primary text-white px-2">
+              {main_hero.title.two}
+            </span>{" "}
+            {main_hero.title.three}
           </h1>
         </div>
         <div className="flex flex-col gap-8 leading-10">
+          <p>{main_hero.text.one}</p>
           <p>
-            Wenn das Morgenlicht den Horizont färbt, erwacht ein neues
-            Wohngefühl. «Aurora» verbindet die Dynamik urbanen Lebens mit der
-            Ruhe des Grüns. Hier vereinen sich Weitblick und Innovation mit
-            moderner Behaglichkeit.
-          </p>
-          <p>
-            In Spreitenbach entstehen{" "}
-            <span className="font-bold">Wohnungen zur Erstvermietung</span>, in
-            denen der Tag mit Licht durchflutet beginnt und die Nacht in
-            angenehmer Atmosphäre endet. 
+            {main_hero.text.two}{" "}
+            <span className="font-bold">{main_hero.text.three}</span>{" "}
+            {main_hero.text.four}
           </p>
         </div>
       </aside>
